cfe644faef4ff353c2d9e1c14ee75a93939f040c
RaginiKumari <58027796+melodyni@users.noreply.github.com>
2020-01-14 15:36:34 +0530

    mySnakeGame

---
+++ gameElements.js
@@ -0,0 +1,127 @@
+class Game {
+  constructor(snake, ghostSnake, food, gridSize, score) {
+    this.snake = snake;
+    this.ghostSnake = ghostSnake;
+    this.food = food;
+    this.previousFood = new Food(1, 1, 1, "normalFood");
+    this.gridSize = gridSize;
+    this.score = score;
+  }
+
+  hasFoodEaten() {
+    const headOfSnake = [this.snake.head];
+    const foodPosition = this.food.positions;
+    return isInSameCell(headOfSnake, foodPosition);
+  }
+
+  generateFood() {
+    this.previousFood = this.food;
+    const colId = getRandomNumUnder(NUM_OF_COLS);
+    const rowId = getRandomNumUnder(NUM_OF_ROWS);
+    this.food = new Food(colId, rowId, 1, "food");
+    if (this.score % 10 === 0) {
+      this.food = new Food(colId, rowId, 5, "powerFood");
+    }
+  }
+
+  hasTouchedEdges() {
+    const [gridWidth, gridHeight] = this.gridSize;
+    const [headCol, headRow] = this.snake.head;
+    const touchedHorizontalEdge = 0 > headCol || headCol > gridWidth;
+    const touchedVerticalEdge = 0 > headRow || headRow > gridHeight;
+
+    return touchedHorizontalEdge || touchedVerticalEdge;
+  }
+
+  update() {
+    if (this.hasFoodEaten()) {
+      this.score = this.score + this.food.point;
+      this.snake.grow();
+      this.generateFood();
+    }
+    this.snake.move();
+  }
+
+  isGameOver() {
+    return this.snake.hasTouchedItself() || this.hasTouchedEdges();
+  }
+}
+
+class Direction {
+  constructor(initialHeading) {
+    this.heading = initialHeading;
+    this.deltas = {};
+    this.deltas[EAST] = [1, 0];
+    this.deltas[WEST] = [-1, 0];
+    this.deltas[NORTH] = [0, -1];
+    this.deltas[SOUTH] = [0, 1];
+  }
+
+  get delta() {
+    return this.deltas[this.heading];
+  }
+
+  turnLeft() {
+    this.heading = (this.heading + 1) % 4;
+  }
+}
+
+class Snake {
+  constructor(positions, direction, type) {
+    this.positions = positions.slice();
+    this.direction = direction;
+    this.type = type;
+    this.previousTail = [0, 0];
+  }
+
+  get head() {
+    return this.positions[this.positions.length - 1];
+  }
+
+  get location() {
+    return this.positions.slice();
+  }
+
+  get species() {
+    return this.type;
+  }
+
+  turnLeft() {
+    this.direction.turnLeft();
+  }
+
+  move() {
+    const [headX, headY] = this.head;
+    this.previousTail = this.positions.shift();
+    const [deltaX, deltaY] = this.direction.delta;
+    this.positions.push([headX + deltaX, headY + deltaY]);
+  }
+
+  grow() {
+    this.positions.unshift(this.previousTail);
+  }
+
+  hasTouchedItself() {
+    const bodyOfSnake = this.location.slice(0, -1);
+    return isInSameCell(bodyOfSnake, this.head);
+  }
+}
+
+class Food {
+  constructor(colId, rowId, energy, type) {
+    this.colId = colId;
+    this.rowId = rowId;
+    this.energy = energy;
+    this.type = type;
+  }
+
+  get positions() {
+    return [this.colId, this.rowId];
+  }
+  get point() {
+    return this.energy;
+  }
+  get quality() {
+    return this.type;
+  }
+}
---
+++ snake.js
@@ -0,0 +1,150 @@
+const EAST = 0;
+const NORTH = 1;
+const WEST = 2;
+const SOUTH = 3;
+
+const getRandomNumUnder = function(number) {
+  return Math.floor(Math.random() * number);
+};
+
+const isInSameCell = function(body, cell) {
+  const [cellCol, cellRow] = cell;
+  return body.some(([col, row]) => col === cellCol && row === cellRow);
+};
+
+const NUM_OF_COLS = 100;
+const NUM_OF_ROWS = 60;
+
+const GRID_ID = "grid";
+const SCORE = "progress";
+
+const getGrid = () => document.getElementById(GRID_ID);
+const getCellId = (colId, rowId) => colId + "_" + rowId;
+
+const getCell = (colId, rowId) =>
+  document.getElementById(getCellId(colId, rowId));
+
+const createCell = function(grid, colId, rowId) {
+  const cell = document.createElement("div");
+  cell.className = "cell";
+  cell.id = getCellId(colId, rowId);
+  grid.appendChild(cell);
+};
+
+const createGrids = function() {
+  const grid = getGrid();
+  for (let y = 0; y < NUM_OF_ROWS; y++) {
+    for (let x = 0; x < NUM_OF_COLS; x++) {
+      createCell(grid, x, y);
+    }
+  }
+};
+
+const fillScores = function(score) {
+  const cell = document.getElementById(SCORE);
+  cell.style.width = `${score}%`;
+};
+
+const eraseTail = function(snake) {
+  let [colId, rowId] = snake.previousTail;
+  const cell = getCell(colId, rowId);
+  cell.classList.remove(snake.species);
+};
+
+const drawSnake = function(snake) {
+  snake.location.forEach(([colId, rowId]) => {
+    const cell = getCell(colId, rowId);
+    cell.classList.add(snake.species);
+  });
+};
+
+const clearFood = function(food) {
+  let [colId, rowId] = food.positions;
+  const cell = getCell(colId, rowId);
+  cell.classList.remove(food.type);
+};
+
+const drawFood = function(food) {
+  const [colId, rowId] = food.positions;
+  const cell = getCell(colId, rowId);
+  cell.classList.add(food.type);
+};
+
+const handleKeyPress = snake => {
+  snake.turnLeft();
+};
+
+const moveAndDrawSnake = function(snake) {
+  eraseTail(snake);
+  drawSnake(snake);
+};
+
+const attachEventListeners = snake => {
+  document.body.onkeydown = handleKeyPress.bind(null, snake);
+};
+
+const setUp = function(game) {
+  attachEventListeners(game.snake);
+  createGrids();
+  drawSnake(game.snake);
+  drawSnake(game.ghostSnake);
+  drawFood(game.food);
+};
+
+const animateSnake = function(snake, ghostSnake) {
+  moveAndDrawSnake(snake);
+  moveAndDrawSnake(ghostSnake);
+};
+
+const randomlyTurnSnake = function(snake) {
+  let x = Math.random() * 100;
+  if (x > 50) {
+    snake.turnLeft();
+  }
+};
+
+const initSnake = function() {
+  const snakePosition = [
+    [40, 25],
+    [41, 25],
+    [42, 25]
+  ];
+  return new Snake(snakePosition, new Direction(EAST), "snake");
+};
+
+const initGhostSnake = function() {
+  const ghostSnakePosition = [
+    [40, 30],
+    [41, 30],
+    [42, 30]
+  ];
+  return new Snake(ghostSnakePosition, new Direction(EAST), "ghost");
+};
+
+const drawUpdatedGame = function(game) {
+  animateSnake(game.snake, game.ghostSnake);
+  randomlyTurnSnake(game.ghostSnake);
+  clearFood(game.previousFood);
+  drawFood(game.food);
+  fillScores(game.score);
+};
+
+const main = function() {
+  const snake = initSnake();
+  const ghostSnake = initGhostSnake();
+  const food = new Food(45, 25, 1, "food");
+  const game = new Game(snake, ghostSnake, food, [99, 59], 0);
+  setUp(game);
+  const runGame = setInterval(() => {
+    game.update();
+    if (game.isGameOver()) {
+      alert("GAME OVER");
+      clearInterval(runGame);
+      return;
+    }
+    if (game.score >= NUM_OF_COLS) {
+      clearInterval(runGame);
+    }
+    drawUpdatedGame(game);
+  }, 100);
+};
